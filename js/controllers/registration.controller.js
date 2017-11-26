712a8206561cf439442fb4dd2169d655cc581cf2
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-11-25 20:04:56 -0600

    added login/registration controllers with validation of input.

--- js/controllers/registration.controller.js
+++ js/controllers/registration.controller.js
@@ -3,7 +3,18 @@
 myApp.controller('RegistrationController', ['$scope', function($scope){
     //The view should now be able to use this variable.
     //"Welcome to my App" will be inserted where {{ message }} is in login.html.  
-    $scope.message = "Welcome to my Registration App";
+    //$scope.message = "Welcome to my Registration App";
 
+    //the login() that is called when the submit button is clicked on the login.html page.
+    //This is the login() method called in login.html.
+    //It is a function literal.
+    $scope.login = function () {
+      $scope.message = "Welcome " +  $scope.user.email;
+    };
+
+    //This is the register() function/method that is called when submit is clicked on the register.html page. 
+    $scope.register = function() { 
+    $scope.message = "Welcome " +  $scope.user.firstname;
+    };
 
 }]); //end of RegistrationController
